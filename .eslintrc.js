b21ac46b04d21664a39cf431539c0d1081935c87
jasonhsieh <appleoxxo@gmail.com>
2019-02-24 13:31:42 +0800

    add eslint prettier precommit hook

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'plugin:jest/recommended', 'prettier', 'prettier/react'],
+  plugins: ['prettier', 'jest', 'json'],
+  env: {
+    'jest/globals': true,
+  },
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js'],
+      },
+    ],
+    'import/no-extraneous-dependencies': 0,
+    'jsx-a11y/no-static-element-interactions': 0, // <div onClick={() => {}} />
+    'react/forbid-prop-types': 0,
+    'react/require-default-props': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
+    ],
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/mouse-events-have-key-events': 0,
+    'no-param-reassign': 0,
+    // Prettier
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
